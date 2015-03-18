2f90cc195efb98617607e3b2dda67222a9fdf620
Kris Brown <kris@kris.me.uk>
2015-03-18 23:41:26 +0000

    store original truncate value and restore in tests

--- test/index.js
+++ test/index.js
@@ -137,12 +137,13 @@ describe('expect()', function () {
     it('asserts on invalid condition (large array, display not truncated)', function (done) {
 
         var exception = false;
+        var origTruncate = Code.truncate;
         try {
             Code.truncate = false;
             Code.expect([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]).to.be.a.string();
         }
         catch (err) {
-            Code.truncate = true;
+            Code.truncate = origTruncate;
             exception = err;
         }
 
@@ -167,12 +168,13 @@ describe('expect()', function () {
     it('asserts on invalid condition (large object, display not truncated)', function (done) {
 
         var exception = false;
+        var origTruncate = Code.truncate;
         try {
             Code.truncate = false;
             Code.expect({ a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10 }).to.be.a.string();
         }
         catch (err) {
-            Code.truncate = true;
+            Code.truncate = origTruncate;
             exception = err;
         }
 
@@ -197,12 +199,13 @@ describe('expect()', function () {
     it('asserts on invalid condition (long object values, display not truncated)', function (done) {
 
         var exception = false;
+        var origTruncate = Code.truncate;
         try {
             Code.truncate = false;
             Code.expect({ a: 12345678901234567890, b: 12345678901234567890 }).to.be.a.string();
         }
         catch (err) {
-            Code.truncate = true;
+            Code.truncate = origTruncate;
             exception = err;
         }
 
@@ -227,12 +230,13 @@ describe('expect()', function () {
     it('asserts on invalid condition (long string, display not truncated)', function (done) {
 
         var exception = false;
+        var origTruncate = Code.truncate;
         try {
             Code.truncate = false;
             Code.expect('{ a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10 }').to.be.a.number();
         }
         catch (err) {
-            Code.truncate = true;
+            Code.truncate = origTruncate;
             exception = err;
         }
 
